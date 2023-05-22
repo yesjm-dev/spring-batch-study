f5a5dd7008d971586dbee5a421917f51ab0e47a5
jeongmin78 <wudals0708@naver.com>
2023-05-23 00:56:20 +0900

    - job 여러개 등록하기

--- src/main/kotlin/com/example/springbatchstudy/config/MyBatchConfig.kt
+++ src/main/kotlin/com/example/springbatchstudy/config/MyBatchConfig.kt
@@ -13,6 +13,7 @@ import org.springframework.batch.item.ItemProcessor
 import org.springframework.batch.item.ItemWriter
 import org.springframework.batch.item.database.JpaPagingItemReader
 import org.springframework.batch.item.database.builder.JpaPagingItemReaderBuilder
+import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -22,21 +23,24 @@ import org.springframework.transaction.PlatformTransactionManager
 class MyBatchConfig(
     private val entityManagerFactory: EntityManagerFactory,
 ): DefaultBatchConfiguration() {
-    companion object { const val chuckSize = 3 }
 
-    @Bean
-    fun myJob(jobRepository: JobRepository, step: Step): Job {
+    companion object {
+        const val chuckSize = 3
+        const val JOB_NAME = "MY_JOB"
+        const val STEP_NAME = "MY_STEP"
+    }
+
+    @Bean(name = [JOB_NAME])
+    fun myJob(jobRepository: JobRepository, @Qualifier("MY_STEP") step: Step): Job {
         return JobBuilder("myJob", jobRepository)
             .start(step)
             .build()
     }
 
-    @Bean
+    @Bean(name = [STEP_NAME])
     fun myStep(jobRepository: JobRepository, transactionManager: PlatformTransactionManager, entityManagerFactory: EntityManagerFactory): Step {
         return StepBuilder("myStep", jobRepository)
             .chunk<Member, String>(chuckSize, transactionManager)
-            // chunk 단위만큼 데이터가 쌓이면 writer에 전달하고, writer는 저장
-            // 마지막 chunk에서는 사이즈 만큼 안채워져도 실행됨
             .reader(reader(null))
             .processor(processor(null))
             .writer(writer(null))
@@ -44,7 +48,7 @@ class MyBatchConfig(
     }
 
     @Bean
-    @StepScope // Bean의 생성 시점이 스프링 애플리케이션이 실행되는 시점이 아닌 @JobScope, @StepScope가 명시된 메서드가 실행될 때까지 지연
+    @StepScope
     fun reader(@Value("#{jobParameters[requestDate]}") requestDate: String?): JpaPagingItemReader<Member> {
         println("==> reader: $requestDate")
         return JpaPagingItemReaderBuilder<Member>()
--- src/main/kotlin/com/example/springbatchstudy/scheduler/MyScheduler.kt
+++ src/main/kotlin/com/example/springbatchstudy/scheduler/MyScheduler.kt
@@ -1,11 +1,11 @@
 package com.example.springbatchstudy.scheduler
 
-import org.springframework.batch.core.Job
-import org.springframework.batch.core.JobExecution
 import org.springframework.batch.core.JobParameter
 import org.springframework.batch.core.JobParameters
+import org.springframework.batch.core.configuration.JobRegistry
+import org.springframework.batch.core.configuration.support.JobRegistryBeanPostProcessor
 import org.springframework.batch.core.launch.JobLauncher
-import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Bean
 import org.springframework.scheduling.annotation.EnableScheduling
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
@@ -13,18 +13,37 @@ import java.time.OffsetDateTime
 
 @EnableScheduling
 @Component
-class MyScheduler {
-    @Autowired lateinit var jobLauncher: JobLauncher
-    @Autowired lateinit var job: Job
+class MyScheduler(
+    private val jobRegistry: JobRegistry,
+    private val jobLauncher: JobLauncher
+) {
+    @Bean
+    fun jobRegistryBeanPostProcessor(): JobRegistryBeanPostProcessor {
+        val postProcessor = JobRegistryBeanPostProcessor()
+        postProcessor.setJobRegistry(jobRegistry)
+        return postProcessor
+    }
 
     @Scheduled(fixedDelay = 30000)
-    fun startJob() {
-        val jobParameterMap = mapOf("requestDate" to JobParameter(OffsetDateTime.now().toString(), String::class.java))
-        val jobParameters = JobParameters(jobParameterMap)
-        val jobExecution: JobExecution = jobLauncher.run(job, jobParameters)
-
-        while (jobExecution.isRunning) {
-            println("isRunning....")
-        }
+    fun startMyJob() {
+        println("======= my job ========")
+        val parameter = mapOf(
+            "requestDate" to JobParameter(OffsetDateTime.now().toString(), String::class.java),
+            "jobName" to JobParameter("myJob", String::class.java)
+        )
+        val jobParameters = JobParameters(parameter)
+        jobLauncher.run(jobRegistry.getJob("myJob"), jobParameters)
     }
+
+    @Scheduled(fixedDelay = 50000)
+    fun startOtherJob() {
+        println("======= other job ========")
+        val parameter = mapOf(
+            "requestDate" to JobParameter(OffsetDateTime.now().toString(), String::class.java),
+            "jobName" to JobParameter("otherJob", String::class.java)
+        )
+        val jobParameters = JobParameters(parameter)
+        jobLauncher.run(jobRegistry.getJob("otherJob"), jobParameters)
+    }
+
 }
